f40f2108faa4397e01f03e6596d222ef2309f079
addhyan94 <addhyant85@gmail.com>
2026-04-13 21:54:40 +0530

    JavaScipt File Add....

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+// Ensure the DOM is fully loaded before searching for elements
+// window.addEventListener("DOMContentLoaded", () => {
+//     console.log("Hello My Makers.... ");
+
+//     // Pass the ID as a string "buttonn" instead of a variable buttonn
+//     let button1 = document.getElementById("buttonn");
+
+//     if (button1) {
+//         button1.addEventListener("click", (e) => {
+//             alert("button was clicked");
+//         });
+//     } else {
+//         console.error("Element with ID 'buttonn' not found.");
+//     }
+// });
+
+
 console.log("Hello My Makers.... ");
+
+let button1 = document.getElementById("buttonn");
+button1.addEventListener("click",(e)=>{
+    alert("Sorry aabhi add ni hua hai Wait pLease.. ");
+})
+let button2 = document.getElementById("buttonn2");
+button2.addEventListener("click",(e)=>{
+    alert("CV add ni hui hai aabhi ....");
+})
