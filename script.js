608b59692242487f5a9e56c33d6738aaa74147ee
addhyan94 <addhyant85@gmail.com>
2026-04-25 15:59:27 +0530

    Add button 1 JS code

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-// Ensure the DOM is fully loaded before searching for elements
-// window.addEventListener("DOMContentLoaded", () => {
-//     console.log("Hello My Makers.... ");
+        console.log("Hello My Makers.... ");
 
-//     // Pass the ID as a string "buttonn" instead of a variable buttonn
-//     let button1 = document.getElementById("buttonn");
-
-//     if (button1) {
-//         button1.addEventListener("click", (e) => {
-//             alert("button was clicked");
-//         });
-//     } else {
-//         console.error("Element with ID 'buttonn' not found.");
-//     }
-// });
-
-
-console.log("Hello My Makers.... ");
-
-let button1 = document.getElementById("buttonn");
-button1.addEventListener("click",(e)=>{
-    alert("Sorry aabhi add ni hua hai Wait pLease.. ");
-})
-let button2 = document.getElementById("buttonn2");
-button2.addEventListener("click",(e)=>{
-    alert("CV add ni hui hai aabhi ....");
-})
+        let button1 = document.getElementById("buttonn");
+        let btn1_go = document.getElementById("cont_titel");
+        button1.onclick = function () {
+            btn1_go.scrollIntoView({
+                behavior: "smooth"
+            });
+       
+        let button2 = document.getElementById("buttonn2");
+        button2.addEventListener("click", (e) => {
+            alert("CV add ni hui hai aabhi ....");
+        })
+        };
